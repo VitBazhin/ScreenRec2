1c7757c68fa67754a927929b42ccba662fd130e6
Vit Bazhin <bazhin@ya.ru>
2022-02-27 13:10:13 +0300

    US #220226 Task#2 Console GetParams+Start

--- Program.cs
+++ Program.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Drawing;
+using System.Management;
 
-using Accord.Video.FFMPEG;
+
 
 namespace ScreenRec2
 {
@@ -14,9 +11,29 @@ namespace ScreenRec2
         public static void Main(string[] args)
         {
             Console.WriteLine("Start");
+            ManagementObjectSearcher searcher =
+                new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_VideoController");
+            int width = 1920;
+            int height = 1080;
+            foreach (ManagementObject queryObj in searcher.Get())
+            {
+                if (queryObj["CurrentHorizontalResolution"] != null)
+                {
+                    width = int.Parse(queryObj["CurrentHorizontalResolution"].ToString());
+                    height = int.Parse(queryObj["CurrentVerticalResolution"].ToString());
+                    Console.WriteLine(width);
+                    Console.WriteLine(height);
+                    break;
+                }
+            }
+            //foreach (var screen in System.Windows.Forms.Screen.AllScreens)
+            //{
+            //    Console.WriteLine(screen.Bounds.Width);
+            //    Console.WriteLine(screen.Bounds.Height);
+            //}
 
-            new ScreenRecord(new Rectangle(0, 0, 1000, 800), "");
-
+            var screenRecord = new ScreenRecord(new Rectangle(0, 0, width, height), "_");
+            //screenRecord.RecordVideo();
             Console.WriteLine("End");
             Console.ReadLine();
         }
